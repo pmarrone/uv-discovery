ac0bba9caad334cc328dd5cba07f3f9a50c32765
Patricio Marrone <marrone.patricio@gmail.com>
2015-12-09 16:04:20 +0000

    Added author pivot

--- public/spa/facetList/facetList.controller.js
+++ public/spa/facetList/facetList.controller.js
@@ -2,24 +2,8 @@ angular.module("solr-discovery")
 .controller('FacetListController', function (Item, $scope) {
 	function updateItems() {
 		Item.get({q: $scope.search}).$promise.then(function (items) { 
-				$scope.items = items;
-				var authors = [];
-				var currentItem;
-				for (var i = 0; i < items.facet_counts.facet_fields.author_keyword.length; i++) {
-					var item = items.facet_counts.facet_fields.author_keyword[i];
-					if (i % 2 === 0) {
-						currentItem = {
-							author: item
-						};
-						authors.push(currentItem);
-					} else {
-						currentItem.count = item;
-					}
-				}
-				items.authors = authors;
-				console.log("Querying");
-			});
-
+			$scope.items = items;
+		});
 	}
 	
 	$scope.updateItems = updateItems;
